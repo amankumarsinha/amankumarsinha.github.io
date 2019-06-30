f7df90c74bd30b656a7cefd3525d7cac244d345c
aman kumar sinha <48887796+amankumarsinha@users.noreply.github.com>
2019-06-30 12:43:25 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,21 @@
+#to  represent real life data
+#collection of data in key : value pair
+
+user = {'name' : 'aman','age' : 20}
+print(user)
+print(type(user))
+
+# 2 method 
+user1 = dict(name = 'aman',age = 20)
+print(user1)
+print(user1['name'])
+
+# type of data store in dict
+#---> number ,string, list,dict
+
+userinfo = {
+     'name': 'aman',
+     'age': 34,
+     'fav': ['3 ididot','lig','ololo']
+ }
+print(userinfo['fav'])
